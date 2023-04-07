172d8f4da5ace027fc7a67b490b96a7fbe53f4f7
abdulprogramer <125268990+abdulprogramer@users.noreply.github.com>
2023-04-07 12:54:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+// program no 1
+
+let FirstNumber =+prompt("Enter Your First Number")
+let SecondNumber=+prompt("Enter Your Second Number")
+if(FirstNumber>SecondNumber)
+console.log("entered number %d is maximum",FirstNumber)
+else if(FirstNumber<SecondNumber)
+console.log("enterd number %d is maximum",SecondNumber)
+
+// program no 2
+let f=+prompt("Enter Any Number")
+if(f>0)
+console.log("The sign is +")
+else
+console.log("The sign is -")
+
+
+// progrm no  3
+let a =+prompt("Enter Your First Number")
+let b=+prompt("Enter Your Second Number")
+let c =+prompt("Enter Your Third Number")
+let d=+prompt("Enter Your Fourth Number")
+let e =+prompt("Enter Your Fifth Number")
+if(a>b && a>c && a>d && a>e)
+console.log("The Number %d is grater",a)
+else if(b>a && b>c && b>d && b>e)
+console.log("The Number %d is grater",b)
+else if(c>a && c>b && c>d && c>e)
+console.log("The Number %d is grater",c)
+else if(d>a && d>b && d>c && d>e)
+console.log("The Number %d is grater",d)
+else
+console.log("The Number %d is grater",e)
+
+
+
+// program no 4
+let g=0
+for (let g = 0; g <=15; g++) {
+   if(g%2==0)
+   console.log("%d is even",g)
+   else
+    console.log("%d is odd",g)
+}
+
+
+
+// program no 5
+let marks=+prompt("Enter Your Marks")
+if(marks<100)
+console.log("Your Grade is A")
+else if(marks<90)
+console.log("Your Grade is B")
+else if(marks<80)
+console.log("Your Grade is C")
+else if(marks<70)
+console.log("Your Grade is D")
+else 
+console.log("Your Grade is F")
+
+
+// program no 6
+let num=prompt("Enter Your Number")
+ if(num%3==0)
+console.log("Fizz")
+else if(num%5==0)
+console.log("Buzz")
+ else if(num%3==0 && num%5==0)
+console.log("FizzBuzz")
+
+
+// program no 7
+
+for (let a="*"; a<5; a++) {
+    console.log("%s",a)
+    
+    
+}
+    
+
+
+
+
+
+
+
+
+
+
+
